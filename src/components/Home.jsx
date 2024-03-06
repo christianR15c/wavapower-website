bc66a61080cbaf5cc22cfc31dc05e63782cc4335
Christian <habinezac15@gmail.com>
2024-03-06 12:34:01 +0200

    ft: home page bug fix added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,7 @@ const Home = () => {
     <div className="bg-neutralSilver" id="home">
       <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen">
         <Carousel className="w-full mx-auto">
-          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
-            {/* Hero text */}
+          {/* <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
             <div>
               <h1 className="md:text-5xl text-2xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug">
                 We create electricity{' '}
@@ -27,46 +26,21 @@ const Home = () => {
             <div>
               <img src={banner1} alt="banner 1" className="max-h-[100vh]" />
             </div>
-          </div>
-          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
-            <div>
+          </div> */}
+          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row w-[100%] items-center justify-between gap-12 oblique">
+            <div className="w-[80%]">
               <img
                 src={banner2}
                 alt="banner 2"
                 // className="w-24 h-24 object-contain"
               />
             </div>
-
             {/* Hero text */}
             <div>
               <h1 className="md:text-5xl text-2xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug">
                 Lessons and insights{' '}
                 <span className="text-brandSecondary leading-snug">
                   from 3 years
-                </span>
-              </h1>
-              <p className="text-neutralGrey text-base mb-8">
-                Enjoy wireless electricity: reliable and accesible anywhere
-                anytime
-              </p>
-              <button className="btn-primary">Get Now</button>
-            </div>
-          </div>
-          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
-            <div>
-              <img
-                src={banner3}
-                alt="banner 3"
-                // className="w-24 h-24 object-contain"
-              />
-            </div>
-
-            {/* Hero text */}
-            <div>
-              <h1 className="md:md:text-5xl text-2xl text-2xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug">
-                Be the first to get this{' '}
-                <span className="text-brandSecondary leading-snug">
-                  amaziing electricity
                 </span>
               </h1>
               <p className="text-neutralGrey text-base mb-8">
